ebc0289cc4f19f24c97d0f8807b0126d08549b36
Bill Kremer <bkrems12@gmail.com>
2017-01-28 20:17:09 -0600

    added additional fields to database creation.

--- database.sql
+++ database.sql
@@ -2,14 +2,16 @@ CREATE TABLE todos (
 	id SERIAL PRIMARY KEY,
 	task TEXT,
 	task_priority VARCHAR(6),
---	CHECK (task_priority = 'low' OR task_priority = 'high'),
+--	consider CHECK (task_priority = 'Low' OR task_priority = 'High'),
 -- use this command? or restrict in <select> <option>
 	task_created TIMESTAMP,
+	task_due TIMESTAMP,
 	task_completed TIMESTAMP
 	);
 
 
-INSERT INTO todos (task, task_created)
-VALUES 	('Vacuum the living room', '2016-12-28'),
-				('Wash the dishes', '2017-01-10'),
-				('Walk the dog', '2017-01-14');
+
+INSERT INTO todos (task, task_priority, task_created, task_due)
+VALUES 	('Vacuum the living room', 'Low', '2016-12-28', ''),
+				('Wash the dishes', 'High', '2017-01-10', '2017-01-20'),
+				('Walk the dog', '', '2017-01-14', '');
