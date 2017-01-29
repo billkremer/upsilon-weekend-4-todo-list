27f88f3916ef666f2f727cfc1d88c199744dbf05
Bill Kremer <bkrems12@gmail.com>
2017-01-28 21:08:10 -0600

    completed POST, DELETE, PUT, and GET routes.
    also added error checking for empty task data

--- routes/todos.js
+++ routes/todos.js
@@ -9,36 +9,75 @@ var config = { database: "upsilon_todolist" };
 // think of as 'how I connect to DB'
 var pool = new pg.Pool(config);
 
+var verbose = true; // lets messages be turned off for non-error console.logs
 
-router.post("/", function(req, res) {
+router.put('/:id', function (req, res) {
+  if (verbose) console.log(req.body, 'req.body PUT', req.params);
+
+  pool.connect (function (err, client, done) { // done is the function we run after it is done.
+    if (err) {
+      console.log('Error connecting to DB', err);
+      res.sendStatus(500);
+      done();
+    } else {
+      client.query( 'UPDATE todos SET task=$2, task_due=$3, task_priority=$4 WHERE id = $1 RETURNING *',  [req.params.id, req.body.title, req.body.task_due, req.body.task_priority], function (err, result) {
+        done(); // gives the connection back to the db
+        if (err) {
+          console.log('error querying DB', err);
+          res.sendStatus(500);
+        } else {
+          if (verbose) console.log('updated', result);
+          res.send(result.rows);
+        };
+      }); // closes client query
+    }; // closes initial else
+  }); // closes pool.connection
+});
+
+router.post("/", function(req, res) { // adding new todos
+if (verbose) console.log(req.body, 'req.body POST');
+
+    var tempdate = new Date().toISOString();
+  if (req.body.dateDue == "") {
+    req.body.dateDue = tempdate;
+  };
+
+  if (req.body.task == "") {
+    req.body.task = "New Task: " + tempdate.substring(0,10);
+  };
+
   pool.connect(function(err, client, done) {
     if (err) {
       console.log("Error connecting to DB", err);
       res.sendStatus(500);
       done();
     } else {
+
+      var createdNow =  new Date().toISOString();
+
       client.query(
-        "INSERT INTO todos (task, task_priority, task_created, edition, publisher) VALUES ($1, $2, $3, $4, $5) RETURNING *;",
-        [ req.body.title, req.body.author, req.body.published, req.body.edition, req.body.publisher ],
+        "INSERT INTO todos (task, task_priority, task_created, task_due) VALUES ($1, $2, $3, $4) RETURNING *;",
+        [ req.body.task, req.body.priority, createdNow, req.body.dateDue],
         function(err, result) {
           done();
           if (err) {
             console.log("Error querying DB", err);
             res.sendStatus(500);
           } else {
-            console.log("Got info from DB", result.rows);
+            if (verbose) console.log("Got info from DB", result.rows);
             res.send(result.rows);
           }
-        }
-      );
-    }
-  });
-});
+        }  // closes client query return function
+      );  // closes client query
+    }; // closes initial else
+  }); // closes pool.connection
+}); // closes post
 
 
+router.delete("/:id", function(req, res) { // deleting todos
+  if (verbose) console.log(req.body, 'req.body delete');
+  if (verbose) console.log(req.params, 'req.params delete');
 
-
-router.get("/", function(req, res) {
   pool.connect(function(err, client, done) {
     if (err) {
       console.log("Error connecting to database", err);
@@ -46,13 +85,39 @@ router.get("/", function(req, res) {
       done(); // returns the connection
     } else {
       // no error occurred, time to query
-      client.query( "SELECT * FROM todos ORDER BY task", function(err, result) {
+      client.query( "DELETE FROM todos WHERE id = $1", [req.params.id] ,
+      function(err, result) {
           done();
           if (err) {
             console.log("Error querying DB", err);
             res.sendStatus(500);
           } else {
-            console.log("Got info from DB", result.rows);
+            if (verbose) console.log('deleted');
+            res.sendStatus(204);
+          }
+        }  // closes client query return function
+      );  // closes client query
+    }; // closes initial else
+  }); // closes pool.connection
+}); // closes delete
+
+
+
+router.get("/", function(req, res) {  // getting the list of todos
+  pool.connect(function(err, client, done) {
+    if (err) {
+      console.log("Error connecting to database", err);
+      res.sendStatus(500);
+      done(); // returns the connection
+    } else {
+      // no error occurred, time to query
+      client.query( "SELECT * FROM todos ORDER BY task_due ASC", function(err, result) {
+          done();
+          if (err) {
+            console.log("Error querying DB", err);
+            res.sendStatus(500);
+          } else {
+            if (verbose) console.log("Got info from DB", result.rows);
             res.send(result.rows);
           }
         }  // closes client query return function
